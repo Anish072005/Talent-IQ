7647dff1df20c09c79e43d03ab76fd1ef4e2451d
Anish Kumar <anish1059.be22@chitkarauniversity.edu.in>
2026-02-12 19:07:44 +0530

    Server file changes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import {env} from './lib/env.js';
 import path from "path"
+import {connectDB} from './lib/db.js';
+import { errorMonitor } from 'events';
+
 const app=express();
 
 const __dirname=path.resolve();
@@ -16,10 +19,21 @@ app.get('/books',(req,res)=>{
 
 
 if(env.NODE_ENV==="production"){
-app.use(express.static(path.join(__dirname,"../../Frontend/dist")));
+app.use(express.static(path.join(__dirname,"../Frontend/dist")));
 
 app.get("/{*any}",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../..Frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname,"..Frontend","dist","index.html"));
 })
 }
-app.listen(env.PORT,()=>console.log(`Server started at ${env.PORT}`));
+const startserver=async()=>{
+    try {
+        await connectDB();
+         app.listen(env.PORT,()=>{
+        console.log(`✅Server is running on port ${env.PORT}`)
+    })
+    } catch (error) {
+        console.log("❌ Error starting server",errorMonitor);
+    }
+ 
+};
+startserver();
