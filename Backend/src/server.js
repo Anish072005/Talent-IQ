1df5859d9a5815430eda8108a8d1a8dac6b5e089
Anish Kumar <anish1059.be22@chitkarauniversity.edu.in>
2026-02-13 18:29:28 +0530

    speeling mistake

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -27,10 +27,10 @@ app.get('/books',(req,res)=>{
 
 
 if (env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
 
   app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
   });
 }
 const startServer = async () => {
