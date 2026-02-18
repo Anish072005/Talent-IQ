2cc737b9b4d649e642243ae3fb9cfc3201334277
Anish Kumar <anish1059.be22@chitkarauniversity.edu.in>
2026-02-18 23:14:29 +0530

    server file changes

---
+++ Backend/src/middleware/protectRoute.js
@@ -0,0 +1,23 @@
+import {requireAuth} from '@clerk/express'
+
+import User from '../Models/User.js';
+export const protectRoute=[
+    requireAuth({signInUrl:"/sign-in"}), // This middleware will ensure that the user is authenticated
+    async(req,res,next)=>{
+try {
+     const clerkId=req.auth.userId; // Get the authenticated user's ID from Clerk
+     if(!clerkId)return res.status(401).json({msg:"Unauthorrized -Inavlid User"})
+        const user=await User.findOne({clerkId})
+    if(!user)return res.status(401).json({msg:"User not found"})
+      
+      //attach the user to the request
+        req.user=user
+        next();
+
+} catch (error) {
+    console.error("Error in proctectRoute Middleware",error);
+    res.status(500).json({message:"Internal  Server Error"})
+}
+    }
+
+]
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,7 +6,11 @@ import { fileURLToPath } from 'url';
 import cors from "cors";
 import {serve} from "inngest/express";
 import { inngest,functions } from './lib/inngest.js';
+import { clerkClient, clerkMiddleware } from '@clerk/express'
+import {protectRoute} from './middleware/protectRoute.js';
+
 const app=express();
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,29 +18,29 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 //credentials:true means that the server will allows browser to include  cookies on request.
 app.use(cors({origin:env.CLIENT_URL,credentials:true}));    
+app.use(clerkMiddleware()) // Add Clerk middleware to parse authentication information
 
 
 app.use("/api/inngest",serve({client:inngest,functions}))
 
 app.get('/health',(req,res)=>{
+
     res.status(200).json({msg:"hello world"})
 })
 
-
-app.get('/books',(req,res)=>{
-    res.status(200).json({msg:"this is thw books endpoint"})
+app.get('/vedio-calls',protectRoute,(req,res)=>{
+  
+    res.status(200).json({msg:"this is a ProctedRoute"})
 })
 const distPath = path.join(__dirname, "../../Backend/dist");
 
 if (env.NODE_ENV === "production") {
   app.use(express.static(distPath));
 
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(distPath, "index.html"));
   });
 }
-
-
 
 const startServer = async () => {
   try {
